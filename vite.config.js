320109d5daa4b289ec163377f708c1e652bc303a
AndriKls <158137294+AndriKls@users.noreply.github.com>
2025-01-05 01:16:11 +0200

    Created header

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: './projects/third',
+  root: './projects/travelJournal',
   plugins: [react()],
 })
