c7bb22ac028758f65b0930f2c98c2da5b0588dd0
AndriKls <158137294+AndriKls@users.noreply.github.com>
2025-01-05 00:11:03 +0200

    React fun facts

---
+++ projects/third/components/App.jsx
@@ -0,0 +1,10 @@
+import Main from "./Main"
+import Navbar from "./Navbar"
+export default function AppComponent() {
+    return (
+        <>
+            <Navbar />
+            <Main />
+        </>
+    )
+}
---
+++ projects/third/index.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client"
+import AppComponent from "./components/app"
+
+const root = createRoot(document.getElementById("root"))
+
+
+
+root.render(
+    <AppComponent />
+)
